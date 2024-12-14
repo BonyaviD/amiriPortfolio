b102fde5869aaf65dd6c2cd3234c5c23874adff9
Navid Bonyadi <navidprinceofpersia@gmail.com>
2024-12-14 21:22:39 +0330

    update hero section

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -3,8 +3,8 @@ import Waveform from "@/components/Waveform.vue";
 </script>
 
 <template>
-  <div>
-    <div class="hero-section">
+  <div class="background">
+    <div class="hero-section container">
       <div class="hero-content">
         <h1 class="hero-name">Amirhossein Amirikia</h1>
         <h2 class="hero-job">SoundDesigner<br />VoiceActor</h2>
@@ -14,27 +14,32 @@ import Waveform from "@/components/Waveform.vue";
       </div>
       <div class="profile-img"></div>
     </div>
-    <NuxtLink to="">Aboute Me</NuxtLink>
-    <NuxtLink to="">my Works</NuxtLink>
+    <div class="hero-btns container">
+      <NuxtLink class="about-me-btn" to="">Aboute Me</NuxtLink>
+      <NuxtLink class="my-work-btn" to="">my Works</NuxtLink>
+    </div>
   </div>
 </template>
 
 <style scoped>
-.hero-section {
+.background {
   background-color: rgb(207, 196, 196);
+}
+
+.hero-section {
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
   gap: 0 10rem;
-  padding: 5rem;
+  padding-top: 8rem;
+  padding-bottom: 4rem;
 }
 
 .hero-content {
-/* background-color: red; */
-display: flex;
-flex-direction: column;
-justify-content: start;
-align-items: start;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: start;
 }
 
 .hero-name {
@@ -43,9 +48,8 @@ align-items: start;
 }
 
 .hero-job {
-padding: 0.5rem 0;
-color: gray;
-
+  padding: 0.5rem 0;
+  color: gray;
 }
 
 .hero-art {
@@ -53,10 +57,32 @@ color: gray;
 }
 
 .profile-img {
-  width: 30rem;
-  height: 30rem;
+  width: 35rem;
+  height: 35rem;
   background-color: rgb(229, 177, 135);
   border-radius: 1.6rem;
   transform: rotate(-10deg);
 }
+.hero-btns {
+  display: flex;
+  align-items: center;
+  gap: 0 2rem;
+  overflow: hidden;
+  width: calc(100%);
+  height: 8rem;
+}
+.hero-btns a {
+  position: relative;
+  top: 4rem;
+  font-size: 2rem;
+  background-color: white;
+  width: 16rem;
+  height: 10rem;
+  display: flex;
+  align-items: start;
+  justify-content: center;
+  border-radius: 4rem;
+  padding-top: 1.5rem;
+}
+
 </style>
