07e74cad49cb40fafb76f408f0fdda7fcccbb8dd
Navid Bonyadi <navidprinceofpersia@gmail.com>
2024-12-20 14:42:01 +0330

    update project

--- app.vue
+++ app.vue
@@ -2,16 +2,64 @@
 import TheHeader from "@/components/TheHeader.vue";
 import TheFooter from "@/components/TheFooter.vue";
 import HeroSection from "@/components/HeroSection.vue";
+import AboutMeBackground from "@/components/background/AboutMeBackground.vue";
 </script>
 
 <template>
   <div>
     <TheHeader />
     <HeroSection />
+    <div class="about-me-section container">
+      <div class="about-me-title">
+        <AboutMeBackground />
+        <span class="about-me">about me</span>
+      </div>
+      <div class="about-me-content">
+        <div class="about-me-content-text">
+          I am a multimedia arts expert with extensive experience in sound design, voiceover, podcast production, and audio content. Also, by
+          mastering professional voiceover and sound design tools, I can creatively and accurately produce the effects and sounds needed for various
+          projects. In addition, I have experience in professional voiceover and performance for podcasts and audio programs, and I continuously seek
+          to improve the quality of expression and message transmission in the best possible way. My skills in video editing and sound and image
+          mixing allow me to produce engaging and professional video content that tells the story in harmony with the audio content.
+        </div>
+      </div>
+    </div>
     <TheFooter />
   </div>
 </template>
 
 <style scoped>
+.about-me-section {
+  display: flex;
+  gap: 0 15rem;
+  padding: 8rem 5rem;
+}
+.about-me-title {
+  position: relative;
+  padding-top: 8rem;
+}
 
+.about-me {
+  position: absolute;
+  top: 10rem;
+  left: 1rem;
+  font-size: 4rem;
+}
+
+.about-me-content {
+  position: relative;
+  transform: rotate(-5deg);
+  width: clamp(250px, 60%, 550px);
+  height: 400px;
+  border-radius: 24px;
+  background-color: rgb(228, 174, 138);
+  font-size: 2.5rem;
+}
+
+.about-me-content-text {
+  position: absolute;
+  top: -5px;
+  left: 0;
+  transform: rotate(5deg);
+}
 </style>
---
+++ components/background/AboutMeBackground.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+const props = defineProps({
+  color: {
+    type: String,
+    default: "white",
+  },
+  viewBox: {
+    type: String,
+    default: "0 0 24 24",
+  },
+});
+</script>
+
+<template>
+    <svg width="280" height="120" viewBox="0 0 163 77" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M15.5019 72.2036L0.90172 17.8908C-0.081775 14.2322 2.52975 10.5891 6.3104 10.3456L153.769 0.848638C162.665 0.275742 165.69 12.4855 157.556 16.132L23.7508 76.1209C20.3816 77.6314 16.4605 75.7693 15.5019 72.2036Z" fill="url(#paint0_linear_560_21074)"/>
+<defs>
+<linearGradient id="paint0_linear_560_21074" x1="47" y1="17" x2="29.5" y2="86" gradientUnits="userSpaceOnUse">
+<stop offset="0.302601" stop-color="#D3D3D3"/>
+<stop offset="1" stop-color="#E5EFE6"/>
+</linearGradient>
+</defs>
+</svg>
+
+</template>
